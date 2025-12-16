f9dc8f50e417cec424ef0c2aac3dfe8ab38c1924
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-16 12:08:56 +0100

    add my location to featureB class

--- src/FeatureB.java
+++ src/FeatureB.java
@@ -1,6 +1,6 @@
 public class FeatureB {
 
     public static void main(String[] args) {
-        System.out.println("Das ist Feature B von: Deniz Waßmuth");
+        System.out.println("Das ist Feature B von: Deniz Waßmuth (Hamburg)add");
     }
 }
